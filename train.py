82cdd4fe635ad5751e5e6c9dd74d64be2f6c76db
Christopher Brückner <bruckner@ufal.mff.cuni.cz>
2025-04-08 15:52:02 +0200

    feat: add training script stub

--- train.py
+++ train.py
@@ -1,6 +1,21 @@
+import torch
 from dataloading import load_merge_encode
+from models import Model
+from transformers import DataCollatorWithPadding, Trainer, TrainingArguments, enable_full_determinism
 from utils import parse_args
 
+
 if __name__ == "__main__":
+    enable_full_determinism(seed=0)
+
     args = parse_args()
-    train_df, test_df = load_merge_encode(args)
+    dataset = load_merge_encode(args)
+    model = Model(args)
+
+    for split in dataset.values():
+        split.map(
+            lambda sample: model.tokenizer(sample["text"], padding=True, truncation=True, return_tensors="pt"),
+            batched=True,
+        )
+
+    collator = DataCollatorWithPadding(model.tokenizer)
