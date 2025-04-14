c1568f6085e8fbb07c6a4b102a2f8cd7eeb2080b
Christopher Brückner <bruckner@ufal.mff.cuni.cz>
2025-04-14 19:41:06 +0200

    refactor: remove utils

--- train.py
+++ train.py
@@ -1,22 +1,53 @@
+import argparse
 from datetime import datetime
 
 import torch
-from torchmetrics.classification import MultilabelF1Score
-from transformers import DataCollatorWithPadding, Trainer, TrainingArguments, enable_full_determinism
+from torchmetrics.classification import BinaryF1Score, MultilabelF1Score
+from transformers import (
+    DataCollatorWithPadding,
+    Trainer,
+    TrainingArguments,
+    XLMRobertaConfig,
+    XLMRobertaTokenizerFast,
+    enable_full_determinism,
+)
 
 from dataloading import load_merge_encode
-from models import Model
-from utils import parse_args
+from hierarchy import create_full_hierarchy, create_taxonomy
+from models import MultilabelModel
+
+LANGUAGE_SETS = {
+    "shared_task": ["bg", "hr", "pl", "sl", "ru"],  # primary languages
+    "slavic": ["bg", "hr", "mk", "pl", "sl", "ru"],  # add Macedonian (close to Bulgarian)
+    "slavic_en": ["bg", "en", "hr", "mk", "pl", "sl", "ru"],  # add English
+    "european_latin": ["bg", "de", "en", "es", "fr", "it", "hr", "mk", "pl", "sl", "ru"],  # add all Latin alphabet
+    "parlamint": ["bg", "de", "el", "en", "es", "fr", "it", "hr", "ka", "mk", "pl", "sl", "ru"],  # add Greek
+    "european": ["bg", "de", "el", "en", "es", "fr", "it", "hr", "ka", "mk", "pl", "sl", "ru"],  # add Georgian
+    "all": ["ar", "bg", "de", "el", "en", "es", "fr", "it", "hr", "ka", "mk", "pl", "sl", "ru"],  # add Arabic
+}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--languages", "-l", choices=LANGUAGE_SETS.keys(), default="slavic")
+    parser.add_argument("--include_clef", "-ic", action="store_true")
+    parser.add_argument("--disable_hierarchy", "-dh", action="store_true")
+    parser.add_argument("--val_size", "-v", type=float, default=0.2)
+    parser.add_argument("--model", "-m", default="classla/xlm-r-parla")
+    parser.add_argument("--gnn", "-g", choices=["GCN", "GAT", "HGCN"])
+    return parser.parse_args()
 
 
 def make_multilabel_metrics(num_classes: int):
     metrics = {
+        "hierarchical_f1": BinaryF1Score(),
         "micro_f1": MultilabelF1Score(num_classes, average="micro"),
         "macro_f1": MultilabelF1Score(num_classes, average="macro"),
     }
 
     def compute_metrics(eval_pred):
         logits, labels = eval_pred
+        # TODO: remove ancestors for micro and macro f1
         return {name: metric(torch.Tensor(logits), torch.Tensor(labels)) for name, metric in metrics.items()}
 
     return compute_metrics
@@ -25,16 +56,27 @@ def make_multilabel_metrics(num_classes: int):
 enable_full_determinism(seed=0)
 
 args = parse_args()
-dataset, binarizer = load_merge_encode(args)
-model = Model(args)
+
+match args.hierarchy:
+    case "full":
+        G = create_full_hierarchy()
+    case "taxonomy":
+        G = create_taxonomy()
+    case _:
+        G = None
+
+dataset, binarizer = load_merge_encode(args, G)
+
+config = XLMRobertaConfig.from_pretrained(args.model, num_labels=len(binarizer.classes_))
+model = MultilabelModel(args, config, G)
+tokenizer = XLMRobertaTokenizerFast.from_pretrained(args.model)
+data_collator = DataCollatorWithPadding(tokenizer)
 
 for split in dataset.keys():
     dataset[split] = dataset[split].map(
-        lambda sample: model.tokenizer(sample["text"], padding=True, truncation=True, return_tensors="pt"),
+        lambda sample: tokenizer(sample["text"], padding=True, truncation=True, return_tensors="pt"),
         batched=True,
     )
-
-data_collator = DataCollatorWithPadding(model.tokenizer)
 
 training_args = TrainingArguments(
     output_dir=f"{args.model}-{args.gnn}-{datetime.now().strftime('%y%m%d%H%M')}",
@@ -49,7 +91,7 @@ training_args = TrainingArguments(
     save_total_limit=1,
     load_best_model_at_end=True,
     fp16=True,
-    metric_for_best_model="f1",
+    metric_for_best_model="macro_f1",
 )
 
 trainer = Trainer(
@@ -58,8 +100,8 @@ trainer = Trainer(
     data_collator=data_collator,
     train_dataset=dataset["train"],
     eval_dataset=dataset["val"],
-    processing_class=model.tokenizer,
-    compute_metrics=make_multilabel_metrics(len(binarizer.classes_)),
+    processing_class=tokenizer,
+    compute_metrics=make_multilabel_metrics(num_classes=config.num_labels),
 )
 
 trainer.train()
