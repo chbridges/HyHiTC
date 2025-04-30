3ba0e1fb3e819d1c0251f0e3787849debc658de3
Christopher Brückner <bruckner@ufal.mff.cuni.cz>
2025-04-30 12:48:33 +0200

    refactor: move configuration into new module

---
+++ config.py
@@ -0,0 +1,52 @@
+import argparse
+
+LANGUAGE_SETS = {
+    "shared_task": ["bg", "hr", "pl", "ru", "sl"],  # primary languages
+    "slavic": ["bg", "hr", "mk", "pl", "ru", "sl"],  # add Macedonian (close to Bulgarian)
+    "slavic_en": ["bg", "en", "hr", "mk", "pl", "ru", "sl"],  # add English
+    "european_latin": ["bg", "de", "en", "es", "fr", "it", "hr", "mk", "pl", "ru", "sl"],  # add all Latin alphabet
+    "parlamint": ["bg", "de", "el", "en", "es", "fr", "it", "hr", "ka", "mk", "pl", "ru", "sl"],  # add Greek
+    "european": ["bg", "de", "el", "en", "es", "fr", "it", "hr", "ka", "mk", "pl", "ru", "sl"],  # add Georgian
+    "all": ["ar", "bg", "de", "el", "en", "es", "fr", "it", "hr", "ka", "mk", "pl", "ru", "sl"],  # add Arabic
+}
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", "-b", type=int, default=16)
+    parser.add_argument("--debug", "-d", action="store_true")
+    parser.add_argument("--epochs", "-e", type=int, default=10)
+    parser.add_argument("--freeze", "-f", action="store_true")
+    parser.add_argument("--gnn", "-g", choices=["gcn", "hgcn", "hie"], default="gcn")
+    parser.add_argument("--hierarchy", "-hi", choices=["full", "taxonomy"])
+    parser.add_argument("--hp_search", "-hp", action="store_true")
+    parser.add_argument("--languages", "-l", choices=LANGUAGE_SETS.keys(), default="european_latin")
+    parser.add_argument("--learning_rate", "-lr", type=float, default=1e-5)
+    parser.add_argument("--model", "-m", default="classla/xlm-r-parla")
+    parser.add_argument("--node_classification", "-nc", action="store_true")
+    parser.add_argument("--node_dim", "-nd", type=int, default=32)
+    parser.add_argument("--pooling", "-p", action="store_true")
+    parser.add_argument("--seed", "-s", type=int, default=42)
+    parser.add_argument("--test_data", "-test", default="slavicnlp2025")
+    parser.add_argument("--train_data", "-train", default="semeval2021,semeval2023,semeval2024")
+    parser.add_argument("--translations", "-mt", action="store_true")
+    parser.add_argument("--val_size", "-v", type=float, default=0.2)
+    return parser.parse_args()
+
+
+def add_hie_arguments(args: argparse.Namespace) -> argparse.Namespace:
+    args = argparse.Namespace(**vars(args))  # shallow copy
+    vars(args)["act"] = "relu"  # Shimizu et al.: ops in hyperbolic space are inherently non-linear
+    vars(args)["c"] = None  # Charmi et al.: learnable curvature leads to performance gain
+    vars(args)["device"] = "cuda:0"
+    vars(args)["dim"] = args.node_dim
+    vars(args)["local-agg"] = 1  # Chami et al.: local tangent space aggregation outperforms agg at origin
+    vars(args)["manifold"] = "Hyperboloid"  # Nickel and Kiela: more numerically stable than Poincaré
+    vars(args)["num-layers"] = 1  # for comparison with HiAGM
+    vars(args)["n-heads"] = 1  # number of attention heads for graph attention networks, must be a divisor dim
+    vars(args)["pos-weight"] = (0,)  # mandatory argument of NCModel but not used
+    vars(args)["use-att"] = 1  # Charmi et al.: attention leads to performance gain
+    if args.gnn == "hie":
+        vars(args)["hyp-ireg"] = "hir_tangent"  # used in Yang et al. paper
+        vars(args)["ireg-lambda"] = 0.1  # Yang et al. do not report their used value
+    return args
--- train.py
+++ train.py
@@ -1,8 +1,8 @@
-import argparse
 from datetime import datetime
 from typing import Optional
 
 import networkx as nx
+import numpy as np
 import torch
 from geoopt.optim import RiemannianAdam
 from sklearn.preprocessing import MultiLabelBinarizer
@@ -17,41 +17,10 @@ from transformers import (
 )
 
 from classifier import HieRoberta
+from config import LANGUAGE_SETS, add_hie_arguments, parse_args
 from dataloading import load_merge_encode
 from hierarchy import create_full_hierarchy, create_taxonomy
-
-LANGUAGE_SETS = {
-    "shared_task": ["bg", "hr", "pl", "ru", "sl"],  # primary languages
-    "slavic": ["bg", "hr", "mk", "pl", "ru", "sl"],  # add Macedonian (close to Bulgarian)
-    "slavic_en": ["bg", "en", "hr", "mk", "pl", "ru", "sl"],  # add English
-    "european_latin": ["bg", "de", "en", "es", "fr", "it", "hr", "mk", "pl", "ru", "sl"],  # add all Latin alphabet
-    "parlamint": ["bg", "de", "el", "en", "es", "fr", "it", "hr", "ka", "mk", "pl", "ru", "sl"],  # add Greek
-    "european": ["bg", "de", "el", "en", "es", "fr", "it", "hr", "ka", "mk", "pl", "ru", "sl"],  # add Georgian
-    "all": ["ar", "bg", "de", "el", "en", "es", "fr", "it", "hr", "ka", "mk", "pl", "ru", "sl"],  # add Arabic
-}
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", "-b", type=int, default=16)
-    parser.add_argument("--debug", "-d", action="store_true")
-    parser.add_argument("--epochs", "-e", type=int, default=10)
-    parser.add_argument("--freeze", "-f", action="store_true")
-    parser.add_argument("--gnn", "-g", choices=["gcn", "hgcn", "hie"], default="gcn")
-    parser.add_argument("--hierarchy", "-hi", choices=["full", "taxonomy"])
-    parser.add_argument("--hp_search", "-hp", action="store_true")
-    parser.add_argument("--languages", "-l", choices=LANGUAGE_SETS.keys(), default="european_latin")
-    parser.add_argument("--learning_rate", "-lr", type=float, default=1e-5)
-    parser.add_argument("--model", "-m", default="classla/xlm-r-parla")
-    parser.add_argument("--node_classification", "-nc", action="store_true")
-    parser.add_argument("--node_dim", "-nd", type=int, default=64)
-    parser.add_argument("--pooling", "-p", action="store_true")
-    parser.add_argument("--seed", "-s", type=int, default=42)
-    parser.add_argument("--test_data", "-test", default="slavicnlp2025")
-    parser.add_argument("--train_data", "-train", default="semeval2021,semeval2023,semeval2024")
-    parser.add_argument("--translations", "-t", action="store_true")
-    parser.add_argument("--val_size", "-v", type=float, default=0.2)
-    return parser.parse_args()
+from PersuasionNLPTools.config import VALID_LABELS
 
 
 def make_multilabel_metrics(
@@ -59,32 +28,41 @@ def make_multilabel_metrics(
     hierarchy: Optional[nx.DiGraph] = None,
 ):
     if hierarchy:
-        leaves = [v for v in hierarchy.nodes() if hierarchy.out_degree(v) == 0]
-        leave_idx = [i for i, c in enumerate(binarizer.classes_) if c in leaves]
+        # leaves = [v for v in hierarchy.nodes() if hierarchy.out_degree(v) == 0]
+        leaves = VALID_LABELS
+        leaves_idx = [i for i, c in enumerate(binarizer.classes_) if c in VALID_LABELS]
         num_leaves = len(leaves)
     else:
-        num_leaves = len(binarizer.classes_)
-        leave_idx = list(range(num_leaves))
+        num_leaves = len(VALID_LABELS)
+        leaves_idx = None
 
-    hierarchical_f1 = MultilabelF1Score(binarizer.classes_, average="micro")
+    label2id = {c: i for i, c in enumerate(binarizer.classes_)}
+
+    hierarchical_f1 = MultilabelF1Score(len(binarizer.classes_), average="micro")
     micro_f1 = MultilabelF1Score(num_leaves, average="micro")
     macro_f1 = MultilabelF1Score(num_leaves, average="macro")
 
-    def add_ancestors(binary_labels):
+    def add_ancestors(y: torch.Tensor):
         if not hierarchy:
-            return binary_labels
+            return y
+        paths = nx.all_pairs_shortest_path_length(hierarchy.reverse())
+        for target, distances in paths:
+            ix_rows = np.where(y[:, label2id[target]] > 0)[0]
+            ancestors = [label2id[k] for k in distances.keys()]
+            y[tuple(np.meshgrid(ix_rows, ancestors))] = 1
+        return y
 
     def compute_metrics(eval_pred):
         logits, labels = eval_pred
-        preds = torch.sigmoid(logits) > 0.5
+        preds = torch.sigmoid(torch.Tensor(logits)) > 0.5
         # add ancestors for hierarchical F1
-
+        preds_with_ancestors = torch.Tensor(add_ancestors(preds))
+        labels_with_ancestors = torch.Tensor(add_ancestors(labels))
         # remove ancestors for macro/micro F1
-        preds_leaves = torch.Tensor(preds[:, leave_idx])
-        labels_leaves = torch.Tensor(labels[:, leave_idx])
-
+        preds_leaves = torch.Tensor(preds[:, leaves_idx])
+        labels_leaves = torch.Tensor(labels[:, leaves_idx])
         return {
-            "hierarchical_f1": hierarchical_f1(torch.Tensor(logits), torch.Tensor(labels)),
+            "hierarchical_f1": hierarchical_f1(preds_with_ancestors, labels_with_ancestors),
             "micro_f1": micro_f1(preds_leaves, labels_leaves),
             "macro_f1": macro_f1(preds_leaves, labels_leaves),
         }
@@ -112,6 +90,8 @@ if __name__ == "__main__":
     print(f"Experiment: {experiment_name}\n")
     for k, v in args.__dict__.items():
         print(f"{k}:\t{v}")
+    if args.gnn in ["hgcn", "hie"]:
+        args = add_hie_arguments()
     print("\nStart Time:", start_time)
 
     match args.hierarchy:
@@ -130,10 +110,13 @@ if __name__ == "__main__":
         val_size=args.val_size,
     )
 
+    id2label = dict(enumerate(binarizer.classes_))
+    label2id = {c: i for i, c in id2label.items()}
+
     if args.debug:
         dataset["train"] = dataset["train"].shard(num_shards=10, index=1)
 
-    config = XLMRobertaConfig.from_pretrained(args.model, num_labels=len(binarizer.classes_))
+    config = XLMRobertaConfig.from_pretrained(args.model, id2label=id2label, label2id=label2id)
     # model = HieRoberta(args, config, G)
     tokenizer = XLMRobertaTokenizerFast.from_pretrained(args.model)
     data_collator = DataCollatorWithPadding(tokenizer)
@@ -185,4 +168,10 @@ if __name__ == "__main__":
 
     results = trainer.evaluate(dataset["test"], metric_key_prefix="test")
     print(results)
+
+    test_logits = trainer.predict(dataset["test"]).predictions
+    test_probas = torch.sigmoid(torch.Tensor(test_logits))
+    test_preds = [np.where(row > 0.5)[0] for row in test_probas]
+    test_labels = [id2label[p] for p in test_preds[0]]
+
     print("Total execution time:", datetime.now() - start_time)
