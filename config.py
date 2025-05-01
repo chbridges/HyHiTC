d2380bd9ea3128140caf53cf7baaa96a84292815
Christopher Brückner <bruckner@ufal.mff.cuni.cz>
2025-05-02 00:43:37 +0200

    fix: add missing HGCN/HIE arguments

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import argparse
 
+from transformers import XLMRobertaConfig
+
 LANGUAGE_SETS = {
     "shared_task": ["bg", "hr", "pl", "ru", "sl"],  # primary languages
     "slavic": ["bg", "hr", "mk", "pl", "ru", "sl"],  # add Macedonian (close to Bulgarian)
@@ -17,12 +19,13 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--debug", "-d", action="store_true")
     parser.add_argument("--epochs", "-e", type=int, default=10)
     parser.add_argument("--freeze", "-f", action="store_true")
-    parser.add_argument("--gnn", "-g", choices=["gcn", "hgcn", "hie"], default="gcn")
-    parser.add_argument("--hierarchy", "-hi", choices=["full", "taxonomy"])
+    parser.add_argument("--gnn", "-g", choices=["GCN", "HGCN", "HIE"])
+    parser.add_argument("--hierarchy", "-hi", choices=["full", "taxonomy"], default="full")
     parser.add_argument("--hp_search", "-hp", action="store_true")
-    parser.add_argument("--languages", "-l", choices=LANGUAGE_SETS.keys(), default="european_latin")
+    parser.add_argument("--language_model", "-lm", default="classla/xlm-r-parla")
+    parser.add_argument("--languages", "-lang", choices=LANGUAGE_SETS.keys(), default="european_latin")
+    parser.add_argument("--layers", "-l", type=int, default=2)
     parser.add_argument("--learning_rate", "-lr", type=float, default=1e-5)
-    parser.add_argument("--model", "-m", default="classla/xlm-r-parla")
     parser.add_argument("--node_classification", "-nc", action="store_true")
     parser.add_argument("--node_dim", "-nd", type=int, default=32)
     parser.add_argument("--pooling", "-p", action="store_true")
@@ -34,20 +37,25 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def add_hie_arguments(args: argparse.Namespace, feat_dims: int) -> argparse.Namespace:
+def add_hyp_default_args(args: argparse.Namespace, config: XLMRobertaConfig) -> argparse.Namespace:
     args = argparse.Namespace(**vars(args))  # shallow copy
     vars(args)["act"] = "relu"  # Shimizu et al.: ops in hyperbolic space are inherently non-linear
+    vars(args)["bias"] = 1
     vars(args)["c"] = None  # Charmi et al.: learnable curvature leads to performance gain
-    vars(args)["device"] = "0"  # cuda:0
+    vars(args)["cuda"] = "0"
+    vars(args)["device"] = "cuda:0"
     vars(args)["dim"] = args.node_dim
-    vars(args)["feat_dims"] = feat_dims  # XLM-R output size
+    vars(args)["dropout"] = config.hidden_dropout_prob
+    vars(args)["feat_dim"] = config.hidden_size  # XLM-R output size
     vars(args)["local_agg"] = 1  # Chami et al.: local tangent space aggregation outperforms agg at origin
-    vars(args)["manifold"] = "Hyperboloid"  # Nickel and Kiela: more numerically stable than Poincaré
+    vars(args)["hyp_ireg"] = "hir_tangent" if args.gnn == "HIE" else "0"  # Yang et al.: comparable with hire_tangent
+    vars(args)["ireg_lambda"] = 0.1  # Yang et al. do not report their used value
+    vars(args)["manifold"] = "PoincareBall"  # Nickel and Kiela: Hyperboloid more numerically stable than Poincaré
+    vars(args)["model"] = args.gnn
+    vars(args)["n_classes"] = len(config.id2label)
     vars(args)["num_layers"] = 1  # for comparison with HiAGM
     vars(args)["n_heads"] = 1  # number of attention heads for graph attention networks, must be a divisor dim
-    vars(args)["pos_weight"] = (0,)  # mandatory argument of NCModel but not used
+    vars(args)["pos_weight"] = None  # mandatory argument of NCModel but not used
+    vars(args)["task"] = ("nc" if args.node_classification else "rec",)
     vars(args)["use_att"] = 1  # Charmi et al.: attention leads to performance gain
-    if args.gnn == "hie":
-        vars(args)["hyp_ireg"] = "hir_tangent"  # used in Yang et al. paper
-        vars(args)["ireg_lambda"] = 0.1  # Yang et al. do not report their used value
     return args
