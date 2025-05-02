ac7d7eb1b1253d202f6cbc0cad203a4128d49306
Christopher Brückner <bruckner@ufal.mff.cuni.cz>
2025-05-03 01:46:31 +0200

    chore: apply code bug fix in HIE merging script

--- merge_hie.sh
+++ merge_hie.sh
@@ -2,3 +2,4 @@ mv ./HIE/layers ./layers
 mv ./HIE/manifolds ./manifolds
 mv ./HIE/models ./models
 mv ./HIE/utils ./utils
+sed -i 's/torch.spmm(adj, x_tangent)/torch.matmul(adj, x_tangent)/' ./layers/hyp_layers.py
